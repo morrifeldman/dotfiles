2feee5bbba5a750fbec75b843442785170df4b15
Morris Feldman <morrifeldman@gmail.com>
2013-12-26 02:57:21 +0200

    0;95;0c

--- bashrc.bash
+++ bashrc.bash
@@ -1,3 +1,4 @@
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export LEIN_ROOT="TRUE"
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [[ $- != *i* ]] && return
 [[ -z "$TMUX" ]] && exec tmux -2
