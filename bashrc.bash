a279f82c17d0720a3e5eb506c915fba946fb199d
Morris Feldman <morrifeldman@gmail.com>
2014-01-11 21:51:31 +0200

    Trying to fix ssh tramp

--- bashrc.bash
+++ bashrc.bash
@@ -1,6 +1,6 @@
 export LEIN_ROOT="TRUE"
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-[[ $- != *i* ]] && return
+[ -z "$PS1" ] && return
 #[[ -z "$TMUX" ]] && (tmux -2 attach || tmux -2 new-session)
 echo "Starting TMUX"
 [[ -z "$TMUX" ]] && exec tmux -2
