0fbd87fbbc422c24f7491c25379ab60fb08d8a93
Morris Feldman <morrifeldman@gmail.com>
2014-01-11 22:01:19 +0200

    Still fixing tramp

--- bashrc.bash
+++ bashrc.bash
@@ -1,7 +1,7 @@
 export LEIN_ROOT="TRUE"
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [ -z "$PS1" ] && return
-[[ $TERM == dumb ]] return
+[[ $TERM -eq dumb ]] && return
 #[[ -z "$TMUX" ]] && (tmux -2 attach || tmux -2 new-session)
 echo "Starting TMUX"
 [[ -z "$TMUX" ]] && exec tmux -2
